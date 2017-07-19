396889da29744bce866df645454eeafb724fb5a3
ThiagoRAD <thauroo@gmail.com>
2017-07-19 09:51:25 -0300

    update

--- src/tasks/TaskList.jsx
+++ src/tasks/TaskList.jsx
@@ -1,17 +1,18 @@
 import LiquidGlass from 'liquid-glass-react'
 import useTaskStore from './store/useTaskStore';
 import TaskItem from './TaskItem';
+import useEditStore from './store/useEditStore'
 
 const TaskList = () => {
   const {tasks, stageSize} = useTaskStore();
+  const {editMode} = useEditStore();
 
   return (
-    <div className='flex flex-col gap-4 p-4'>
+    <div className='p-4'>
       {tasks.slice(0, stageSize).map((task) => (
             <TaskItem key={task.id} task={task} isStaged />
       ))}
-      <hr />
-      {tasks.slice(stageSize).map((task) => (
+      {editMode && tasks.slice(stageSize).map((task) => (
           <TaskItem key={task.id} task={task} />
       ))}
     </div>
