a805d5b4162cae01225b696238fb14727f167870
ThiagoRAD <thauroo@gmail.com>
2017-10-07 13:40:04 -0300

    updating

--- src/tasks/store/useTaskStore.js
+++ src/tasks/store/useTaskStore.js
@@ -4,15 +4,16 @@ import { persist } from 'zustand/middleware';
 const useTaskStore = create(
   persist(
     (set) => ({
-      pool: [],
-      staging: [],
+      tasks: [],
       stageSize: 5,
       decreaseStageSize: () => set((state) => ({ stageSize: Math.max(1, state.stageSize - 1) })),
       increaseStageSize: () => set((state) => ({ stageSize: Math.min(5, state.stageSize + 1) })),
-      addToStage: (tasks) => set((state) => ({ staging: [...state.staging, ...tasks] })),
-      removeFromStage: (task) => set((state) => ({ staging: state.staging.filter((t) => t.id !== task.id) })),
-      addToPool: (task) => set((state) => ({ pool: [...state.pool, task] })),
-      removeFromPool: (task) => set((state) => ({ pool: state.pool.filter((t) => t.id !== task.id) })),
+      addTask: (tasks) => set((state) => ({ tasks: [...state.tasks, ...tasks] })),
+      removeTask: (id) => set((state) => ({ tasks: state.tasks.filter((task) => task.id !== id) })),
+      updateTask: (updatedTask) =>
+        set((state) => ({
+          tasks: state.tasks.map((task) => (task.id === updatedTask.id ? updatedTask : task)),
+        })),
     }),
     {
       name: 'overflow-task-storage',
