5dbc6be54c271002f0febfd5fa8200c381f9d645
ThiagoRAD <thauroo@gmail.com>
2017-11-06 10:54:47 -0300

    updating

--- src/tasks/TaskList.jsx
+++ src/tasks/TaskList.jsx
@@ -1,5 +1,36 @@
-const TaskList = () => {
-  return <div className="task-list">Task List Component</div>
-}
+import {useEffect} from 'react';
+import useTaskStore from './store/useTaskStore';
+import TaskItem from './TaskItem';
 
-export default TaskList
+const TaskList = () => {
+  const {staging, pool, stageSize, addToStage} = useTaskStore();
+
+  useEffect(() => {
+    const replenishStaging = () => {
+      const toAdd = [];
+      while (staging.length < stageSize) {
+        const poolElementsNotInStaging = pool.filter((task) => !staging.find((stagedTask) => stagedTask.id === task.id));
+        if (poolElementsNotInStaging.length === 0) {
+          break;
+        }
+        poolElementsNotInStaging.sort((a, b) => b.createdAt - a.createdAt);
+        const taskToAdd = poolElementsNotInStaging[0];
+        toAdd.push(taskToAdd);
+      }
+      if(toAdd.length > 0)
+        addToStage(toAdd);
+    };
+    if(staging.length < stageSize)
+      replenishStaging();
+  });
+
+  return (
+    <div className='task-list'>
+      {staging.map((task) => (
+        <TaskItem key={task.id} task={task} />
+      ))}
+    </div>
+  );
+};
+
+export default TaskList;
