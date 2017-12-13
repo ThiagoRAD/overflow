f4e82fd51232d0ecb9fc3c20e5d8507a63c6abd5
ThiagoRAD <thauroo@gmail.com>
2017-12-12 22:32:18 -0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = ({children}) => {
   const cyclicTasks = tasks.filter(task => task.type === 'Cyclic').length
   return (
     <header className="flex text-white items-center space-between">
-      <div className="gap-4 p-4 text-sm flex items-end text-[8px]"> {stageSize} of {totalTasks} tasks<span className="text-gray-400 flex items-center"> ({dailyTasks} <RiRepeatLine /> --- {cyclicTasks} <AiOutlineClockCircle/></span> </div>
+      <div className="gap-4 p-4 text-sm flex items-end text-[8px]"> {parseInt(stageSize)} of {totalTasks} tasks<span className="text-gray-400 flex items-center"> ({dailyTasks} <RiRepeatLine /> --- {cyclicTasks} <AiOutlineClockCircle/></span> </div>
       <div className="flex text-white items-stretch justify-end gap-4 p-4 flex-1">
         {children}
       </div>
