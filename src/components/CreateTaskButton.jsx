e6b477c3c88d8ea8ccbadc4ef3146ab1566ab7f2
ThiagoRAD <thauroo@gmail.com>
2018-01-24 09:51:25 -0300

    update

--- src/components/CreateTaskButton.jsx
+++ src/components/CreateTaskButton.jsx
@@ -1,6 +1,7 @@
 import {Link, useNavigate} from 'react-router-dom';
 import './CreateTaskButton.css';
 import GlassButton from './GlassButton';
+import { BiPlus } from 'react-icons/bi'
 
 const CreateTaskButton = () => {
 
@@ -8,7 +9,7 @@ const CreateTaskButton = () => {
 
   return (
     <GlassButton onClick={() => navigate("/create-task")}>
-      +
+      <BiPlus />
     </GlassButton>
   );
 };
