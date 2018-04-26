03d8e7d855981547d97ea64d056b45261e26289a
ThiagoRAD <thauroo@gmail.com>
2018-04-26 11:15:02 -0300

    removing from stage

--- src/tasks/TaskItem.jsx
+++ src/tasks/TaskItem.jsx
@@ -1,5 +1,12 @@
-const TaskItem = () => {
-  return <div className="task-item">Task Item Component</div>
+import useTaskStore from './store/useTaskStore'
+
+const TaskItem = ({task}) => {
+  const { removeFromStage } = useTaskStore()
+
+  return <div className="task-item" onClick={() => removeFromStage(task)}>
+    <h3>{task.name}</h3>
+    <p>Duration: {task.duration} minutes</p>
+  </div>
 }
 
 export default TaskItem
