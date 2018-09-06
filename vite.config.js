9f334d92105ec7a553c3c2c1c97bb1de69083119
ThiagoRAD <thauroo@gmail.com>
2018-09-06 12:10:28 -0300

    including pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['./public/overflow.jpg', './public/icon512_maskable.png', './public/icon512_rounded.png'],
+      manifest: {
+        name: "Overflow",
+        short_name: "Overflow",
+        themeColors: "#000000",
+        icons: [
+          {
+            src: 'icon512_maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'icon512_rounded.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ],
   base: '/overflow',
 })
