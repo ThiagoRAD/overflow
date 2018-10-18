49af0a2b1f441944c212dc820a769bee11184189
ThiagoRAD <thauroo@gmail.com>
2018-10-18 15:20:31 -0300

    adding a basic pomodoro

--- src/tasks/TaskList.jsx
+++ src/tasks/TaskList.jsx
@@ -1,48 +1,14 @@
-import {useEffect} from 'react';
 import useTaskStore from './store/useTaskStore';
 import TaskItem from './TaskItem';
 
-const importanceWeight = {
-  1: 1,
-  2: 1.2,
-  3: 1.5,
-  4: 1.8,
-  5: 2,
-};
+
 
 const TaskList = () => {
-  const {staging, pool, stageSize, addToStage} = useTaskStore();
-
-  useEffect(() => {
-    const getNextFromPool = () => {
-      const availablePoolItems = pool.filter((task) => !staging.find((stagedTask) => stagedTask.id === task.id));
-      let next = null;
-      for (let task of availablePoolItems) {
-        if (next === null) {
-          next = task;
-          continue;
-        }
-        const today = new Date().getTime();
-        const nextTaskUrgency = (today - next.tackledAt) * importanceWeight[next.importance];
-        const taskUrgency = (today - task.tackledAt) * importanceWeight[task.importance];
-        if (taskUrgency > nextTaskUrgency) {
-          next = task;
-        }
-      }
-      return next;
-    };
-
-    if (staging.length < stageSize) {
-      const nextTask = getNextFromPool();
-      if (nextTask) {
-        addToStage([nextTask]);
-      }
-    }
-  }, [staging, pool, stageSize, addToStage]);
+  const {tasks, stageSize} = useTaskStore();
 
   return (
     <div className='task-list'>
-      {staging.map((task) => (
+      {tasks.slice(0, stageSize).map((task) => (
         <TaskItem key={task.id} task={task} />
       ))}
     </div>
