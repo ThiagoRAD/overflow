cd1a4b21aef6903603c17e1bcb48a5e1f9cb5ba9
ThiagoRAD <thauroo@gmail.com>
2019-05-22 10:03:45 -0300

    base files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import './App.css'
+import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import NextTasks from './tasks/NextTasks'
+import CreateTask from './tasks/CreateTask'
+import SelectedTask from './tasks/SelectedTask'
 
 const _overflowWork = {
-  name: "Overflow", 
-  duration: "30 min",
-  icon: "🌊",
+  name: 'Overflow',
+  duration: '30 min',
+  icon: '🌊',
   priority: 2,
   timesCompleted: 0,
   currentSession: {
     inProgress: false,
     startTime: null,
-    endTime: null
+    endTime: null,
   },
-}
+};
 
 const App = () => {
-  return <div className="background">aaaaaaaaaaaaaaaaaaaaaaaa</div>
-  
-}
+  return (
+    <div className='background'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<NextTasks />} />
+          <Route path='/create-task' element={<CreateTask />} />
+          <Route path='/task/:id' element={<SelectedTask />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
