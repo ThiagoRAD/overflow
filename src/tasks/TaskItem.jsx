d9e4ff733053b7a7862f5305777ca1354aab19ff
ThiagoRAD <thauroo@gmail.com>
2019-09-22 00:15:50 -0300

    update

--- src/tasks/TaskItem.jsx
+++ src/tasks/TaskItem.jsx
@@ -1,11 +1,14 @@
 import { useNavigate } from 'react-router-dom'
 import useEditStore from './store/useEditStore'
 import Pomodoro from './Pomodoro'
+import useTagsStore from './store/useTagsStore'
+import { MdLabel } from 'react-icons/md'
 
 const TaskItem = ({ task }) => {
   const navigate = useNavigate()
   const {editMode} = useEditStore();
-
+  const { tags } = useTagsStore()
+  const tag = tags.find(tag => tag.tasks.includes(task.id))
   const onClick = () => {
     if(editMode)
       navigate(`/create-task/${task.id}`)  
@@ -25,6 +28,7 @@ const TaskItem = ({ task }) => {
       <div className="flex items-center gap-2 text-sm">
         <div className="w-12 h-12 relative">
           <div className='absolute right-6 w-15 text-[10px]'>{task.tackledAt && new Date(task.tackledAt).toLocaleString("en-US", { month: 'short', day: 'numeric' })}</div>
+          <div className='absolute flex gap-1 items-center justify-end right-13 bottom-0 text-[10px]'>{tag ? <><MdLabel style={{ color: tag.color }} /> {tag.name}</> : ''}</div>
           <Pomodoro task={task} />
         </div>
       </div>
