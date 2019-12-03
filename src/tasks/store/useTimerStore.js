afcced247e4d233d6e32e3c524b400a26cf82e6a
ThiagoRAD <thauroo@gmail.com>
2019-12-03 13:40:05 -0300

    updating

--- src/tasks/store/useTimerStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-const useTimerStore = create((set) => ({
-  selectedTask: null,
-  startPomodoro: (task) => set({ selectedTask: task }),
-  stopPomodoro: () => set({ selectedTask: null }),
-}));    
-
-export default useTimerStore;
