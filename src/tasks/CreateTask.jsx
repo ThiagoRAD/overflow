d1eb186405db25c402bb47d01f11a2f09fa0bf68
ThiagoRAD <thauroo@gmail.com>
2020-03-20 10:54:47 -0300

    updating

--- src/tasks/CreateTask.jsx
+++ src/tasks/CreateTask.jsx
@@ -1,61 +1,45 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import useTaskStore from './store/useTaskStore'
 import './CreateTask.css'
 
 const CreateTask = () => {
+  const [name, setName] = useState('')
+  const [duration, setDuration] = useState(30)
+  const [importance, setImportance] = useState(3)
   const navigate = useNavigate()
-  const [task, setTask] = useState({
-    name: '',
-    icon: '📝',
-    duration: 30,
-    importance: 3
-  })
+  const { addToPool } = useTaskStore()
 
-  const handleSubmit = (e) => {
+  const createTask = (e) => {
     e.preventDefault()
-    // TODO: Save task to state/storage
-    console.log('Task created:', task)
+    const id = crypto.randomUUID()
+    console.log(id)
+    addToPool({ id, name, duration, importance, createdAt: new Date().getTime() })
     navigate('/')
-  }
-
-  const handleChange = (field, value) => {
-    setTask(prev => ({ ...prev, [field]: value }))
   }
 
   return (
     <div className="create-task">
       <h2>Create New Task</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={createTask}>
         
         <div className="form-group">
           <label>Task Name *</label>
           <input
             type="text"
-            value={task.name}
-            onChange={(e) => handleChange('name', e.target.value)}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             required
             placeholder="Enter task name"
           />
         </div>
 
         <div className="form-group">
-          <label>Icon</label>
-          <input
-            type="text"
-            className="icon-input"
-            value={task.icon}
-            onChange={(e) => handleChange('icon', e.target.value)}
-            placeholder="Choose an emoji"
-          />
-          <small>Popular: 🌊 📝 💻 🏃 📚 🎯 💡 ✅</small>
-        </div>
-
-        <div className="form-group">
           <label>Duration (minutes) *</label>
           <input
             type="number"
-            value={task.duration}
-            onChange={(e) => handleChange('duration', parseInt(e.target.value))}
+            value={duration}
+            onChange={(e) => setDuration(+e.target.value)}
             required
             min="1"
             max="240"
@@ -63,20 +47,15 @@ const CreateTask = () => {
         </div>
 
         <div className="form-group">
-          <label>Importance: {task.importance}</label>
+          <label>Importance: {importance}</label>
           <input
             type="range"
             className="importance-slider"
-            value={task.importance}
-            onChange={(e) => handleChange('importance', parseInt(e.target.value))}
+            value={importance}
+            onChange={(e) => setImportance(+e.target.value)}
             min="1"
             max="5"
           />
-          <div className="importance-labels">
-            <span>1 (Low)</span>
-            <span>3 (Medium)</span>
-            <span>5 (High)</span>
-          </div>
         </div>
 
         <div className="button-group">
