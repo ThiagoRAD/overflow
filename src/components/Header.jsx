3eaaeb779f537a8d3c956cfb244e4b0d3577e134
ThiagoRAD <thauroo@gmail.com>
2020-04-28 00:30:22 -0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
+import useTaskStore from '../tasks/store/useTaskStore'
+
 const Header = ({children}) => {
+
+
+  const { tasks, stageSize } = useTaskStore()
+  const totalTasks = tasks.length
+
   return (
-    <header className="flex text-white items-stretch justify-end gap-4 p-4">
-      {children}
+    <header className="flex text-white items-center space-between">
+      <div className="gap-4 p-4">{stageSize} / {totalTasks} </div>
+      <div className="flex text-white items-stretch justify-end gap-4 p-4 flex-1">
+        {children}
+      </div>
     </header>
   );
 }
