2d0414d32190f204902a6bea038c553dba4b64fd
ThiagoRAD <thauroo@gmail.com>
2020-11-03 13:40:04 -0300

    updating

--- src/tasks/SelectedTask.jsx
+++ src/tasks/SelectedTask.jsx
@@ -1,4 +1,12 @@
+import { useParams } from 'react-router-dom'
+import useTaskStore from './store/useTaskStore'
+
 const SelectedTask = () => {
+
+  const id = useParams().id
+  const { pool } = useTaskStore()
+  const task = pool.find((t) => t.id === id)
+
   return <div className="selected-task">Selected Task Component</div>
 }
 
