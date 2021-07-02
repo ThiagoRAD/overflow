65aa54e40a6be4230e6bb2c97012edc456d52bb6
ThiagoRAD <thauroo@gmail.com>
2021-07-02 13:40:04 -0300

    updating

--- src/tasks/TaskList.jsx
+++ src/tasks/TaskList.jsx
@@ -2,29 +2,43 @@ import {useEffect} from 'react';
 import useTaskStore from './store/useTaskStore';
 import TaskItem from './TaskItem';
 
+const importanceWeight = {
+  1: 1,
+  2: 1.2,
+  3: 1.5,
+  4: 1.8,
+  5: 2,
+};
+
 const TaskList = () => {
   const {staging, pool, stageSize, addToStage} = useTaskStore();
 
   useEffect(() => {
-    const replenishStaging = () => {
-      const toAdd = [];
-      const elementsToReplenish = stageSize - staging.length;
-      if (elementsToReplenish) {
-        const poolElementsNotInStaging = pool.filter((task) => !staging.find((stagedTask) => stagedTask.id === task.id));
-        if (poolElementsNotInStaging.length === 0) {
-          return;
+    const getNextFromPool = () => {
+      const availablePoolItems = pool.filter((task) => !staging.find((stagedTask) => stagedTask.id === task.id));
+      let next = null;
+      for (let task of availablePoolItems) {
+        if (next === null) {
+          next = task;
+          continue;
         }
-        poolElementsNotInStaging.sort((a, b) => a.createdAt - b.createdAt);
-        const taskToAdd = poolElementsNotInStaging.slice(0, elementsToReplenish);
-        toAdd.push(...taskToAdd);
+        const today = new Date().getTime();
+        const nextTaskUrgency = (today - next.tackledAt) * importanceWeight[next.importance];
+        const taskUrgency = (today - task.tackledAt) * importanceWeight[task.importance];
+        if (taskUrgency > nextTaskUrgency) {
+          next = task;
+        }
       }
-      if(toAdd.length > 0)
-        console.log("Calling add to stage")
-        addToStage(toAdd);
+      return next;
     };
-    if(staging.length < stageSize)
-      replenishStaging();
-  });
+
+    if (staging.length < stageSize) {
+      const nextTask = getNextFromPool();
+      if (nextTask) {
+        addToStage([nextTask]);
+      }
+    }
+  }, [staging, pool, stageSize, addToStage]);
 
   return (
     <div className='task-list'>
