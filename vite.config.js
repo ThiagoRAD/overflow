4e1b9ccee05447107cae59a1bb3166aebe680308
ThiagoRAD <thauroo@gmail.com>
2022-04-30 12:15:50 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -8,29 +8,38 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['./public/overflow.jpg', './public/icon512_maskable.png', './public/icon512_rounded.png'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'pwa-192x192.png', 'pwa-512x512.png'],
       manifest: {
-        name: "Overflow",
+        name: "Overflow - Task Manager",
         short_name: "Overflow",
+        description: "A Pomodoro-based task management app",
         theme_color: '#000000',
         background_color: '#000000',
-        start_url: '/overflow',
-        scope: '/overflow',
+        start_url: '/overflow/',
+        scope: '/overflow/',
         display: 'standalone',
         icons: [
           {
-            src: 'icon512_maskable.png',
-            sizes: '512x512',
+            src: '/overflow/pwa-192x192.png',
+            sizes: '192x192',
             type: 'image/png',
+            purpose: 'any'
           },
           {
-            src: 'icon512_rounded.png',
+            src: '/overflow/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/overflow/maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       }
     })
   ],
-  base: '/overflow',
+  base: '/overflow/',
 })
