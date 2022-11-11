84a55ed80d0c0c68eb2131d1746cfe6baad4ead5
ThiagoRAD <thauroo@gmail.com>
2022-11-11 12:01:25 -0300

    update

--- src/tasks/TaskItem.jsx
+++ src/tasks/TaskItem.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from 'react-router-dom'
-import './TaskItem.css'
 
 const TaskItem = ({task, isStaged}) => {
   const navigate = useNavigate()
@@ -9,10 +8,15 @@ const TaskItem = ({task, isStaged}) => {
       navigate(`/task/${task.id}`)
   }
 
-  return <div className="task-item" onClick={onClick}>
-    <h3>{task.name}</h3>
-    <p>Goal: {task.duration} minutes</p>
-  </div>
+  return (
+    <div 
+      className="p-4 border border-white/10 rounded-2xl cursor-pointer transition-all duration-200 hover:bg-white/5 hover:shadow-lg text-lg"
+      onClick={onClick}
+    >
+      <h3>{task.name}</h3>
+      <p className="text-xs opacity-70">Goal: {task.duration} minutes</p>
+    </div>
+  )
 }
 
 export default TaskItem
