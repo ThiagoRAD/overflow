a7eab022afdf306b47b52d284b02b2b469eb0495
ThiagoRAD <thauroo@gmail.com>
2023-01-01 10:04:23 -0300

    update

--- src/components/GlassButton.jsx
+++ src/components/GlassButton.jsx
@@ -1,5 +1,5 @@
 const GlassButton = ({children, ...props}) => {
-  return <button className="relative overflow-hidden shadow-lg rounded-[--btn-radius] cursor-pointer h-5 w-5"  {...props}>
+  return <button className="relative overflow-hidden shadow-lg rounded-[--btn-radius] cursor-pointer h-5 w-[20px]"  {...props}>
     <div className="absolute inset-0 backdrop-filter-[--btn-filters]"/>
     <div className="absolute inset-0 inline-flex items-center justify-center font-bold text-white bg-[--btn-content-bg]">
         {children}
