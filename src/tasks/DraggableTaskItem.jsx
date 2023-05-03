b46ba0efd55a31d4436fa1a9e3c5c99f5adcfda9
ThiagoRAD <thauroo@gmail.com>
2023-05-03 00:03:52 -0300

    update

--- src/tasks/DraggableTaskItem.jsx
+++ src/tasks/DraggableTaskItem.jsx
@@ -9,20 +9,11 @@ const DraggableTaskItem = ({ task }) => {
 
   return (
     <div 
-      className="mt-4 p-4 border border-white/10 rounded-2xl flex justify-between items-center cursor-pointer transition-all duration-200 hover:bg-white/5 hover:shadow-lg text-lg"
+      className="mt-1 p-1 border border-white/10 rounded-2xl flex justify-between items-center cursor-pointer transition-all duration-200 hover:bg-white/5 hover:shadow-lg text-lg"
       draggable
       onDragStart={onDragStart}
     >
-      <div>
-        <h3>{task.name}</h3>
-        <p className="text-xs opacity-70">Goal: {task.duration} minutes</p>
-      </div>
-      <div className="flex items-center gap-2 text-sm">
-        <div className="w-12 h-12 relative">
-          <div className='absolute right-6 w-15 text-[10px]'>{task.tackledAt && new Date(task.tackledAt).toLocaleString("en-US", { month: 'short', day: 'numeric' })}</div>
-          <Pomodoro task={task} />
-        </div>
-      </div>
+      <h3 className='text-sm'>{task.name}</h3>
     </div>
   )
 }
