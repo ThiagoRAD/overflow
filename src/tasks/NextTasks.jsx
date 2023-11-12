04047c831c5d2bea9d2c08b1d143e23fb6e61a5d
ThiagoRAD <thauroo@gmail.com>
2023-11-12 10:16:04 -0300

    create task button

--- src/tasks/NextTasks.jsx
+++ src/tasks/NextTasks.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom'
+import CreateTaskButton from '../components/CreateTaskButton'
+
 const NextTasks = () => {
-  return <div className="next-tasks">Next Tasks Component</div>
+  return (
+    <div className="next-tasks">
+      <CreateTaskButton />
+      Next Tasks Component
+    </div>
+  )
 }
 
 export default NextTasks
