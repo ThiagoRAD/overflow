24a7d671d192cfabd5b3c1bc73d89eb5a9a1cc6a
ThiagoRAD <thauroo@gmail.com>
2024-05-08 10:54:47 -0300

    updating

--- src/tasks/store/useTaskStore.js
+++ src/tasks/store/useTaskStore.js
@@ -3,7 +3,10 @@ import { create } from 'zustand';
 const useTaskStore = create((set) => ({
   pool: [],
   staging: [],
-  addToStage: (task) => set((state) => ({ staging: [...state.staging, task] })),
+  stageSize: 5,
+  decreaseStageSize: () => set((state) => ({ stageSize: Math.max(1, state.stageSize - 1) })),
+  increaseStageSize: () => set((state) => ({ stageSize: Math.min(5, state.stageSize + 1) })),
+  addToStage: (tasks) => set((state) => ({ staging: [...state.staging, ...tasks] })),
   removeFromStage: (task) => set((state) => ({ staging: state.staging.filter((t) => t.id !== task.id) })),
   addToPool: (task) => set((state) => ({ pool: [...state.pool, task] })),
   removeFromPool: (task) => set((state) => ({ pool: state.pool.filter((t) => t.id !== task.id) })),
