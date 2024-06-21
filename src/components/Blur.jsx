968ab7892a5fcd31e88b5b12adbf55a4cdd9af20
ThiagoRAD <thauroo@gmail.com>
2024-06-20 21:58:19 -0300

    update

---
+++ src/components/Blur.jsx
@@ -0,0 +1,26 @@
+import './Blur.css'
+
+const Blur = ({ children }) => {
+  return <div className="blur-light">
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div className='blur-child' />
+    <div>
+      {children}
+    </div>
+  </div>
+}
+
+
+export default Blur
