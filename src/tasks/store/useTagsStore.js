48d27b7d4964c71250848ec43d1449f349047895
ThiagoRAD <thauroo@gmail.com>
2025-02-13 20:41:36 -0300

    update

---
+++ src/tasks/store/useTagsStore.js
@@ -0,0 +1,32 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import dbStorage from './dbStorage'
+
+const tagsStore = create(
+  persist(
+    (set) => ({
+      tags: [],
+      createTag: (tag) => set((state) => ({ tags: [...state.tags, tag] })),
+      deleteTag: (id) => set((state) => ({ tags: state.tags.filter((tag) => tag.id !== id) })),
+      addTaskToTag: (tagId, taskId) => set((state) => ({
+        tags: state.tags.map((tag) => {
+          if(tag.id !== tagId) return tag;
+          return { ...tag, taskIds: [...(tag.taskIds || []), taskId] }
+        })
+      })),
+      removeTaskFromTag: (tagId, taskId) => set((state) => ({
+        tags: state.tags.map((tag) => {
+          if(tag.id !== tagId) return tag;
+          return { ...tag, taskIds: (tag.taskIds || []).filter(id => id !== taskId) }
+        })
+      })),
+      setTags: (tags) => set(() => ({ tags })),
+    }),
+    {
+      name: 'overflow-tags-storage',
+      storage: createJSONStorage(() => dbStorage),
+    }
+  ) 
+);
+
+export default tagsStore;
