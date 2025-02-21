b784b7498eba6fe1f3663f9356805e115237fe7e
ThiagoRAD <thauroo@gmail.com>
2025-02-21 15:02:27 -0300

    update

--- src/components/GlassButton.jsx
+++ src/components/GlassButton.jsx
@@ -3,7 +3,7 @@ import Blur from './Blur';
 const GlassButton = ({children, ...props}) => {
   console.log(children)
   return (
-    <button className='relative overflow-hidden shadow-lg rounded-[--btn-radius] cursor-pointer h-[250px] w-[250px]' {...props}>
+    <button className='relative overflow-hidden shadow-lg rounded-[--btn-radius] cursor-pointer h-7.5 w-7.5' {...props}>
       <Blur></Blur>
       {/* {/* <div className='absolute inset-0 backdrop-filter-[--btn-filters]' /> */}
       <div className='absolute inset-0 inline-flex items-center justify-center font-bold text-white bg-[--btn-content-bg]'>{children}</div>
