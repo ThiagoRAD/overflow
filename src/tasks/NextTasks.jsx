7c1135b2ff4ab6b29a34185c7dbb619daf0e206c
ThiagoRAD <thauroo@gmail.com>
2025-07-02 09:51:25 -0300

    update

--- src/tasks/NextTasks.jsx
+++ src/tasks/NextTasks.jsx
@@ -1,11 +1,22 @@
 import CreateTaskButton from '../components/CreateTaskButton';
 import TaskList from './TaskList';
 import Header from '../components/Header';
+import EditModeButton from '../components/EditModeButton'
+import useEditStore from './store/useEditStore'
+import { useEffect } from 'react'
 
 const NextTasks = () => {
+
+  const { editMode, toggleEditMode } = useEditStore();
+
+  useEffect(() => {
+    if(editMode) toggleEditMode()
+  }, [])
+
   return (
     <div>
       <Header>
+        <EditModeButton />
         <CreateTaskButton />
       </Header>
       <br />
