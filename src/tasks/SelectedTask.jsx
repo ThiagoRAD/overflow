6de02cfffadf6d2183286765cb5d4e9f13631fdd
ThiagoRAD <thauroo@gmail.com>
2026-01-30 18:24:04 -0300

    update

--- src/tasks/SelectedTask.jsx
+++ src/tasks/SelectedTask.jsx
@@ -1,22 +1,17 @@
 import {useNavigate, useParams} from 'react-router-dom';
-import {useEffect, useRef} from 'react';
+import {useRef} from 'react';
 import useTaskStore from './store/useTaskStore';
 import './SelectedTask.css';
-import useNotification from '../useNotification';
 import Pomodoro from './Pomodoro'
 
 const SelectedTask = () => {
   const id = useParams().id;
-  const {tasks, updateTask, updateTaskTimeRemaining, increaseStageSize, decreaseStageSize, reorder, removeTask} = useTaskStore();
+  const {tasks, updateTask, increaseStageSize, decreaseStageSize, reorder, removeTask} = useTaskStore();
   const task = tasks?.find((t) => t.id === id);
   const intervalRef = useRef(null);
   const navigate = useNavigate();
-  const notification = useNotification();
   
- 
   const wakeLockRef = useRef(null);
-  const hasNotifiedRef = useRef(false);
-
   const requestWakeLock = async () => {
     try {
       wakeLockRef.current = await navigator.wakeLock.request('screen');
@@ -45,53 +40,14 @@ const SelectedTask = () => {
     }
   };
 
-  const timerFinishedEvent = () => {
-    
-    clearInterval(intervalRef.current);
-    intervalRef.current = null;
-    const tackledAt = new Date().getTime();
-    const updatedTask = {...task, tackledAt, ongoing: false, timeRemaining: 0, lastTime: null};
-    updateTask(updatedTask);
-    if (hasNotifiedRef.current) return; 
-    hasNotifiedRef.current = true;
-    notification.notify(`Task "${task.name}" completed!`);
-  };
-
-  const startTimer = () => {
-    intervalRef.current = setInterval(() => {
-      updateTaskTimeRemaining(task.id);
-    }, 1000);
-  };
-
-  useEffect(() => {
-    if (task.ongoing && task.timeRemaining > 0 && !intervalRef.current) {
-      startTimer();
-    }
-    if (!task.ongoing) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = null;
-    }
-
-    if (task.timeRemaining <= 0) {
-      timerFinishedEvent();
-    }
-    return () => {
-      clearInterval(intervalRef.current);
-      intervalRef.current = null;
-    }
-  }, [task.ongoing, task.timeRemaining]);
-
   const handleStart = () => {
-    hasNotifiedRef.current = false; // Reset notification flag when starting
     requestWakeLock();
     updateTask({...task, ongoing: true});
   };
 
   const handlePause = () => {
     updateTask({...task, ongoing: false, lastTime: null});
-  };
-
-  
+  }; 
 
   return (
     <div className='selected-task'>
